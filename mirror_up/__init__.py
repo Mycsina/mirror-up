c2ce59ef3f839ee1daa139cb227920bb9b526725
André Cardoso <andremacardoso@ua.pt>
2022-04-30 01:23:02 +0100

    0.1.5 bump

--- mirror_up/__init__.py
+++ mirror_up/__init__.py
@@ -3,5 +3,5 @@ from mirror_up.mirror_ace import MirrorAceConnection  # noqa
 
 __author__ = """Mycsina"""
 __email__ = "mycsina@protonmail.com"
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 APP_NAME = "mirror-up"
